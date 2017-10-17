ead85ab45dac6a76c9a86fd7659258897cbc3f8b
budaig <imovomi@gmail.com>
2017-10-17 10:19:36 +0800

    update posts

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Decrypt the private key
-openssl aes-256-cbc -K $encrypted_d0af8ee98daa_key -iv $encrypted_d0af8ee98daa_iv -in .travis/codingbdm.enc -out ~/.ssh/codingbdm -d
+openssl aes-256-cbc -K $encrypted_d0af8ee98daa_key -iv $encrypted_d0af8ee98daa_iv -in .travis/4gcbdg.enc -out ~/.ssh/4gcbdg -d
 # Set the permission of the key
-chmod 600 ~/.ssh/codingbdm
+chmod 600 ~/.ssh/4gcbdg
 # Start SSH agent
 eval $(ssh-agent)
 # Add the private key to the system
-ssh-add ~/.ssh/codingbdm
+ssh-add ~/.ssh/4gcbdg
 # Copy SSH config
 cp .travis/ssh_config ~/.ssh/config
 
 # Set Git config
-git config --global user.name "budaim"
-git config --global user.email brooks.chen@aol.com
+git config --global user.name "budaig"
+git config --global user.email brux@qq.com
 # Deploy to GitHub
 hexo deploy
 rm -rf ~/.ssh
