71ceb76bd343e420fbc282ea18871d79abbd4c07
budaig <imovomi@gmail.com>
2019-08-16 12:37:13 +0800

    site update

--- themes/next/source/lib/reading_progress/reading_progress.js
+++ themes/next/source/lib/reading_progress/reading_progress.js
@@ -1,18 +1,30 @@
-(function () {
-	if(!$('.reading-progress-bar').length)return //no progress bar found
+;(function() {
+	var $bar = $('.reading-progress-bar')
+	if (!$bar.length) return //no progress bar found
 	var supportsPassive = false
 	try {
 		var opts = Object.defineProperty({}, 'passive', {
-			get: function () {
+			get: function() {
 				supportsPassive = true
 			}
 		})
-		window.addEventListener("test", null, opts)
-	} catch (e) { }
-
-	$(document).ready(function () {
-		window.addEventListener('scroll', function () {
-			$('.reading-progress-bar').css('width', ($(this).scrollTop() / ($('.post-block').height() - $(this).height()) * 100) + '%')
-		}, supportsPassive ? { passive: true } : false)
+		window.addEventListener('test', $.noop, opts)
+		window.removeEventListener('test', $.noop)
+	} catch (e) {}
+	var $w = $(window)
+	var $d = $(document)
+	$d.ready(function() {
+		window.addEventListener(
+			'scroll',
+			function() {
+				var wh = $w.height()
+				var dh = $d.height()
+				var st = $w.scrollTop()
+				var percent = (st / (dh - wh)) * 100
+				percent = percent > 100 ? 100 : percent < 0 ? 0 : percent
+				$bar.css('width', percent + '%')
+			},
+			supportsPassive ? { passive: true } : false
+		)
 	})
 })()
